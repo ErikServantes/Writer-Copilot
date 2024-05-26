5a373c79aa82fab02c7ccabab5b5aac1e96a6271
ErikServantes <116036335+ErikServantes@users.noreply.github.com>
2024-05-26 14:38:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,21 +6,3 @@ function addCharacter() {
         document.getElementById("characters-list").appendChild(div);
     }
 }
-
-function addWorldDetail() {
-    var detail = prompt("Detalhe do Mundo:");
-    if (detail) {
-        var div = document.createElement("div");
-        div.textContent = "Detalhe: " + detail;
-        document.getElementById("world-details").appendChild(div);
-    }
-}
-
-function addEvent() {
-    var event = prompt("Evento:");
-    if (event) {
-        var div = document.createElement("div");
-        div.textContent = "Evento: " + event;
-        document.getElementById("events-timeline").appendChild(div);
-    }
-}
